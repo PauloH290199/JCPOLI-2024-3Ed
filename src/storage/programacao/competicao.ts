98727b4161053e51fb953d1d72e003b7f93e66d7
Paulo Henrique <55743232+PauloH290199@users.noreply.github.com>
2024-03-24 17:59:49 -0300

    Update competicao.ts
    Atualização dos dados.

--- src/storage/programacao/competicao.ts
+++ src/storage/programacao/competicao.ts
@@ -1,80 +1,27 @@
 const Competicao = [
   {
-    title: "Maratona de Programação - Primeiro Período",
-  instructor: "Alexandre Ribeiro, José Luiz e Olegário",
-  institution: "PUC Goiás",
-  desc: `Com o objetivo de motivar os estudantes da PUC Goiás a expandirem sua capacidade de resolver problemas, utilizando programação de computadores e desenvolverem a habilidade de trabalhar em grupo, a Maratona de Programação da Escola Politécnica e de Artes será realizada como parte integrante da II JCPOLI.
-  `,
-  date: "13/05",
-  time: "08:30 - 12:15",
-  place: "Lab. 101C a 105C, área II",
-  image: "assets/img/slider/logo-maratona.jpeg"
+    title: "xx",
+  instructor: "xx",
+  institution: "xx",
+  desc: `xx`,
+  date: "xx",
+  time: "xx",
+  place: "xx",
+  image: "xx"
   },
-
   {
-    title: "IV DESAFIO EM CIÊNCIA DE DADOS",
-  instructor: "Maria José Pereira Dantas. e José Elmo de Menezes.",
-  institution: "PUC Goiás",
-  desc: `Os participantes enfrentam questões de pesquisa que
-  requerem a aplicação de técnicas de Estatística e aprendizado de máquina (machine
-  learning). Eles devem utilizar seus conhecimentos teóricos e habilidades práticas
-  para analisar uma base de dados pública e encontrar soluções criativas e eficientes
-  para os problemas propostos.
-    Além de promover a troca de experiências entre os participantes, o desafio
-  busca incentivar o desenvolvimento de competências essenciais na área de ciência
-  de dados, como análise estatística, modelagem de dados, interpretação de
-  resultados e comunicação dos achados.`,
-  inscrevase:"https://forms.office.com/r/0YEy9CJ567",
-  datainscri:"08/05 (início às 09:00h) e 09/05(até às 23:59h)",
-  editais: "/assets/Edital-DESAFIO-CIÊNCIA DOS DADOS-VF-2023-0305.pdf",
-  vencedor:"/assets/ivcienciasdados.pdf",
-  date: "",
-  time: "",
-  place: "",
-  image: "assets/img/slider/logo-maratona.jpeg"
-  },
-
-  {
-    title: "I CodeCraft: A Maratona de Desenvolvimento de Sistemas",
-  instructor: "Lucília Gomes Ribeiro. e Solange da Silva",
-  institution: "PUC Goiás",
-  desc: "O CodeCraft é uma maratona de programação (hackathon) que busca estimular a criatividade e o desenvolvimento de soluções tecnológicas inovadoras em um ambiente de competição saudável e colaborativo. O objetivo principal é desafiar os participantes a colocarem em prática suas habilidades criativas, em programação, análise e desenvolvimento de sistemas, com foco em encontrar soluções que possam contribuir para resolver problemas reais e relevantes do cotidiano\n Dia 08:\n 19h às 20h30 - Abertura da Hackathon Como construir um pitch matador (Patrick)\n 20h30 às 22h - Palestra Como soft skills podem lhe garantir uma vaga ou salvar o seu emprego (Camilla)\n Dia 09:\n 17h às 18h30 - Assistir a palestra do youtube IARA: Centro de Pesquisa Aplicada em Inteligência Artificial para Cidades Inteligentes(André Carlos)\n 19h às 20h30 - Assistir a palestra pelo Teams Cidades Inteligentes e Sustentáveis: Ferramentas e Desafios(Raphael)\n 20h30 às 22h - Palestra TI: Caminhos e oportunidades dentro de um mar de possibilidades(Patrick)\n Dia 10:\n 17h às 19h - Oficina de GitHub (Milton) - LAB 101C\n 19h às 21h - HACKATHON\n 21h às 22h - Apresentação dos Pitchs\n Observação: todas as atividades, com exceção da HACKATHON, é aberta ao público.".replace(/\n/g, "<br>"),
-  inscrevase:"https://teams.microsoft.com/l/entity/81fef3a6-72aa-4648-a763-de824aeafb7d/_djb2_msteams_prefix_2939677700?context=%7B%22subEntityId%22%3Anull%2C%22channelId%22%3A%2219%3AO38fAS1tDiX3-WkCufiNboPCD32x8lSh2Ae2m9lS3og1%40thread.tacv2%22%7D&groupId=19da8e1f-0199-4600-8d69-d7a4dbd51135&tenantId=73319f42-8908-4b89-9f8d-558cf4d5d776&allowXTenantAccess=false",
-  datainscri:"05/05 (início às 09:00h) e 09/05(até às 23:59h)",
-  editais:"/assets/Edital-CodeCraft-2023-1.pdf",
-  date: " 08 a 10/05/2023",
-  time: "A partir de 18:45",
-  place: "Auditório 1 (Área II) e Laboratórios 101 a 105C (somente no dia 10/05, das 18h45 às 22h)",
-  image: "assets/img/slider/codecraft.jpg"
-  },
-
-  {
-    title: "Desafio Produtivo",
-    instructor: "Santiago Meireles Rocha e Luciana Casaletti",
-    institution: "PUC Goiás",
-    desc: `Será proposto um desafio para equipes, onde cada uma será responsável por analisar uma das empresas indicadas e defender sua aquisição por parte dos investidores.`,
-    image: "assets/img/slider/DesafioProdutivo.jpg",
-    date: "10/05",
-    time: "09:00-12:15",
-    place: "Sala 211, bloco F, área III",
-  },
-
-  {
-    title: "I Concurso de Fotografia da Escola Politécnica e de Artes",
-    instructor: "LUCIANA CASALETTI e GENILDA DA SILVA ALEXANDRIA SOUSA",
-    institution: "PUC Goiás",
-    desc: `I Concurso de Fotografia da Escola Politécnica e de Artes tem como objetivo fomentar a produção de imagens relacionadas ao tema Escola Politécnica e de Artes: 
-    o que nos une, e contribuir para a integração dos cursos da Escola Politécnica e de Artes da Universidade Católica de Goiás, bem como, com o desenvolvimento da cultura 
-    e arte entre a comunidade acadêmica.`,
-    inscrevase:"https://forms.office.com/r/tmZtXUnWB0",
-    datainscri: "24 de abril a 14 de maio de 2023",
-    comissjulgadora:"/assets/julgadora.pdf",
-    editprorog:"/assets/prorog.pdf",
-    editais:"/assets/Concurso_Artes.pdf",
-    image: "assets/img/slider/expo_foto.jpg",
-    date: "",
-    vencedor:"/assets/premiofotografia.pdf",
-    place: "Hall Área 2 PUC",
+    title: "xx",
+  instructor: "xx",
+  institution: "xx",
+  desc: `xx`,
+  date: "xx",
+  time: `xx`,
+  place: "xx",
+  place2: "xx",
+  place3: "xx",
+  link: "xx",
+  image: "xx",
+  patrocinador: "xx"
   },
 
 ]
